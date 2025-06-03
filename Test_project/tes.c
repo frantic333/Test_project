c4fba312545d0e7eb5e44c23dc59b4e87f7f377c
frantic333 <frantic33@yandex.ru>
2025-06-04 01:52:53 +0300

    creatde test.c

---
+++ Test_project/tes.c
@@ -0,0 +1,13 @@
+//
+//  tes.c
+//  Test_project
+//
+//  Created by Максим Шатурный on 04.06.2025.
+//
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
