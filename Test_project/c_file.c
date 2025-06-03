d5cf1f973c032de67decb84d2dd74a61406579d1
frantic333 <frantic33@yandex.ru>
2025-06-04 02:31:31 +0300

    created c_file

---
+++ Test_project/c_file.c
@@ -0,0 +1,8 @@
+//
+//  c_file.c
+//  Test_project
+//
+//  Created by Максим Шатурный on 04.06.2025.
+//
+
+#include "c_file.h"
---
+++ Test_project/c_file.h
@@ -0,0 +1,13 @@
+//
+//  c_file.h
+//  Test_project
+//
+//  Created by Максим Шатурный on 04.06.2025.
+//
+
+#ifndef c_file_h
+#define c_file_h
+
+#include <stdio.h>
+
+#endif /* c_file_h */
